7ee521db627cf4bc3fe566693c67e94924f02a8a
JLP0578 <j.lepivert@outlook.fr>
2023-04-22 21:35:33 +0200

    [Feat]: Loading.jsx

---
+++ src/components/pages/Loading.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../../assets/styles/pages/Loading.css';
+
+export default class Loading extends React.Component {
+    render() {
+
+        return (
+        <>
+            <section id="preloader">
+                <fieldset className="loading-container">
+                    <legend className="title" data-content="Loading resources">Loading resources</legend>
+                        <div className="loading-bar">
+                            <p className="perc"></p>
+                        </div>
+                </fieldset>
+            </section>
+            <div id="crt"></div>
+        </>
+        );
+    }
+}
+
+Loading.propTypes = {
+    isLoaded: PropTypes.bool
+};
+
+Loading.defaultProps = {
+    isLoaded: false
+};
