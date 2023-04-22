2a9d702a3e2936d341e6b9a0e99cb9bffe63a982
JLP0578 <j.lepivert@outlook.fr>
2023-04-22 21:29:50 +0200

    [Feat]: Footer.jsx

---
+++ src/components/organisms/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import '../../assets/styles/organismes/Footer.css';
+
+export default class Footer extends React.Component {
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <footer>
+                
+            </footer>
+        );
+    }
+}
