c780fe82af99678a3b0cad330f6ca9fc0fded75b
JLP0578 <j.lepivert@outlook.fr>
2023-04-22 21:29:04 +0200

    [Feat]: Icons.jsx

---
+++ src/components/atoms/Icons.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+import '../../assets/styles/atoms/Icons.css';
+
+export default class Icons extends React.Component {
+
+    addHigh(template) {
+        return (
+            <span className={"icons icons_"+template}>+</span>
+        );
+    }
+    addNeutral(template) {
+        return (
+            <span className={"icons icons_"+template}>~</span>
+        );
+    }
+    addLow(template) {
+        return (
+            <span className={"icons icons_"+template}>-</span>
+        );
+    }
+
+    render() {
+        const template = this.props.template || Icons.defaultProps.template;
+
+        let result;
+        switch (template) {
+            case "high":
+                result = this.addHigh(template);
+                break;
+
+            default:
+            case "neutral":
+                result = this.addNeutral(template);
+                break;
+
+            case "low":
+                result = this.addLow(template);
+                break;
+        }
+
+        return result;
+    }
+
+}
+
+Icons.propTypes = {
+    template: PropTypes.string,
+};
+
+Icons.defaultProps = {
+    template: "neural",
+};
