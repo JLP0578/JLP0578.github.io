db4a0cc4f9b1adbd7404d312639d52a1e122ed24
JLP0578 <j.lepivert@outlook.fr>
2023-04-23 12:40:55 +0200

    --dev

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App.jsx';
 
+const globalAny = global;
+
 test('renders learn react link', () => {
   render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
