73a8ec39015c3aebfafd0c82c6d126e5c3ab163d
JLP0578 <j.lepivert@outlook.fr>
2023-09-26 18:50:00 +0200

    [Style]: Temp position

--- src/components/organisms/Temp.jsx
+++ src/components/organisms/Temp.jsx
@@ -9,8 +9,12 @@ export default class Temp extends React.Component {
     render() {
         return (
             <>
-                <h1>Portfolio de Julie LP</h1>
-                <p>Ce site est actuellement en cours de construction.</p>
+                <div className="temporaire">
+                    <div>
+                        <h1>Portfolio de Julie LP</h1>
+                        <p>Ce site est actuellement en cours de construction.</p>
+                    </div>
+                </div>
             </>
         );
     }
